3b3787511a623d311ec93ccdc37b281d22ed5c1c
edchancdn <edchan.cdn@gmail.com>
2022-10-21 16:52:26 -0400

    updated streams

--- src/main/java/streams/StreamRunner.java
+++ src/main/java/streams/StreamRunner.java
@@ -3,7 +3,11 @@ package streams;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
+//import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
 
 public class StreamRunner {
 
@@ -15,18 +19,62 @@ public class StreamRunner {
         dishes.forEach(n -> System.out.println(n));
 
         System.out.println("--- nonVeggies: nonveg, pork, descending prep time ---");
-        List<Dish> nonVeggies = nonVeggies(dishes);
+        List<Dish> nonVeggies = nonVeggiesStream(dishes);
         nonVeggies.forEach(n -> System.out.println(n));
+
+        List<String> uniqueIngredients = getIngredients(dishes);
+        System.out.println(uniqueIngredients);
+
+        List<String> ingredients = dishes.stream()
+                .flatMap(d -> d.getIngredients().stream())
+                .distinct()
+                .collect(toList());
+        System.out.println(ingredients);
+
+        // toSet
+        Set<String> ingredientsSet = dishes.stream()
+                .flatMap(d -> d.getIngredients().stream())
+                .collect(toSet());
+        System.out.println(ingredientsSet);
+
+        Set<String> in = dishes.stream()
+                .flatMap(d -> d.getIngredients().stream())
+                .collect(toSet());
+
     }
 
-    public static List<Dish> nonVeggies(List<Dish> dishes) {
+    public static List<String> addStar(List<String> strings) {
+        return strings.stream().map(s -> s + "*").collect(toList());
+    }
+
+    // flatNap
+    public static List<String> getIngredients(List<Dish> dishes) {
+        Stream<String> dList = dishes.stream()
+                .flatMap(d -> d.getIngredients().stream());
+        List<String> sList = dList.distinct().collect(toList());
+        return sList;
+    }
+
+    public static List<Dish> nonVeggiesStream(List<Dish> dishes) {
         List<Dish> dList = dishes.stream()
                 // include only NONVEG and having an ingredient of pork
                 .filter(d -> (d.getType() == DishType.NONVEG) && (d.getIngredients().contains("pork")))
                 // sort by price and preparationTime, in descending order
                 .sorted(Comparator.comparing(Dish::getPrice)
                         .thenComparing(Dish::getPreparationTime).reversed())
-                .collect(Collectors.toList());
+                .collect(toList());
+        return dList;
+    }
+
+    public static List<Dish> nonVeggiesLoop(List<Dish> dishes) {
+        List<Dish> dList = new ArrayList<>();
+        for (Dish d :dishes) {
+            if (d.getType() == DishType.NONVEG && d.getIngredients().contains("pork")) {
+                dList.add(d);
+            }
+        }
+        dList.sort(Comparator.comparing(Dish::getPrice)
+                .thenComparing(Dish::getPreparationTime).reversed());
         return dList;
     }
 
@@ -52,7 +100,7 @@ public class StreamRunner {
         dishes.add(Dish.builder()
                 .dishId(3)
                 .price(3.00)
-                .preparationTime(30)
+                .preparationTime(25)
                 .type(DishType.NONVEG)
                 .name("Grilled Pork")
                 .ingredients(List.of("pork", "bbq sauce", "spices"))
@@ -61,7 +109,7 @@ public class StreamRunner {
         dishes.add(Dish.builder()
                 .dishId(4)
                 .price(3.00)
-                .preparationTime(20)
+                .preparationTime(30)
                 .type(DishType.NONVEG)
                 .name("Pork Dumpling")
                 .ingredients(List.of("pork", "spices"))
